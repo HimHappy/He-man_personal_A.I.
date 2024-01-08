450b991688bcfb1f64aacb638dc78c6d752e1d9c
Himanshu Rajak <hdhimanshu670@gmail.com>
2024-01-08 23:01:33 +0530

    Update openaitest.py

--- openaitest.py
+++ openaitest.py
@@ -1,5 +1,6 @@
 from openai import OpenAI
 client = OpenAI()
+# add your openai api key to system enviroment that is much efficient
 
 response = client.chat.completions.create(
   model="gpt-3.5-turbo",
